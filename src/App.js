1573d7a5a2b3a872fe14ddc353a08dd546746aff
brunosz19 <hesz.bruno05@gmail.com>
2023-03-04 03:39:05 -0500

    ADD: Make the condition to update the direction of the player

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import Board from "./components/board";
 import { PLAYER_ONE, PLAYER_TWO } from "./config/const";
 import useInterval from "./hooks/useInterval";
@@ -9,7 +9,16 @@ function updateGame(players, action) {
   if (action.type === "move") {
     const newPlayers = players.map(player => ({
       ...player,
-      position: { x: player.position.x + 15, y: player.position.y}
+      position: { x: player.position.x + player.direction.x, y: player.position.y + player.direction.y}
+    }))
+    return newPlayers;
+  }
+
+  if (action.type === 'changeDirection'){
+    const newPlayers = players.map(player => ({
+      ...player,
+      direction: player.keys[action.key] ? player.keys[action.key] : player.direction,
+      position: { x: player.position.x + player.direction.x, y: player.position.y + player.direction.y}
     }))
     return newPlayers;
   }
@@ -21,6 +30,19 @@ function App() {
   useInterval(() => {
     gameDispatch({ type: "move" });
   }, 1000);
+
+  useEffect(() => {
+    function handleKeyPress(event){
+      const key = `${event.keyCode}`;
+      gameDispatch({ type: 'changeDirection', key })
+    }
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return function cleanUp(){
+      document.addEventListener('keydown', handleKeyPress);
+    }
+  }, []);
 
   return (
     <div>
