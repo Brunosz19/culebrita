6c2cb064102b32ce9f712b28ffbd5920926eb09c
brunosz19 <hesz.bruno05@gmail.com>
2023-03-04 03:08:43 -0500

    ADD: useReducer and useInterval to make the players change the position

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useReducer } from "react";
 import Board from "./components/board";
+import { PLAYER_ONE, PLAYER_TWO } from "./config/const";
+import useInterval from "./hooks/useInterval";
+
+const initialState = [PLAYER_ONE, PLAYER_TWO];
+
+function updateGame(players, action) {
+  if (action.type === "move") {
+    const newPlayers = players.map(player => ({
+      ...player,
+      position: { x: player.position.x + 15, y: player.position.y}
+    }))
+    return newPlayers;
+  }
+}
 
 function App() {
+  const [players, gameDispatch] = useReducer(updateGame, initialState);
+
+  useInterval(() => {
+    gameDispatch({ type: "move" });
+  }, 1000);
+
   return (
     <div>
-      <Board />
+      <Board players={ players } />
     </div>
   );
 }
