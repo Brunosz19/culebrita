4b55b4ff1501ca81a8b2444f875cbd527a6d1e4d
brunosz19 <hesz.bruno05@gmail.com>
2023-03-04 00:11:05 -0500

    ADD: new constants to save the players object and to save the size of the board and size of the snake

---
+++ src/config/const.js
@@ -0,0 +1,26 @@
+const UNIT = 15;
+const BOARD_SIZE = 750;
+
+const PLAYER_ONE = {
+  COLOR: "#CC0000",
+  ID: "1",
+  KEYS: {
+    '38': 'up',
+    '39': 'right',
+    '40': 'down',
+    '37': 'left' 
+  }
+}
+
+const PLAYER_TWO = {
+  COLOR: "#CC0000",
+  ID: "2",
+  KEYS: {
+    '87': 'up',
+    '68': 'right',
+    '83': 'down',
+    '65': 'left' 
+  }
+}
+
+export { UNIT, BOARD_SIZE }
