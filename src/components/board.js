dd22247861df26d90699deb05a7ebeddf255857a
brunosz19 <hesz.bruno05@gmail.com>
2023-03-04 00:01:28 -0500

    ADD: new component that will be the board or background

--- src/components/board.js
+++ src/components/board.js
@@ -1,9 +1,43 @@
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useEffect, useRef } from "react";
+
+const unit = 15;
+const boardSize = 750;
+
+const CanvasBoard = styled.canvas`
+  border: 1px solid #777;
+  outline: 1px solid #333;
+  outline-offset: 5px;
+  margin-top: 25px;
+`
+
+function Board(){
+  const canvasRef = useRef();
+
+  
+  useEffect(function() {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.strokeStyle = "white"
+
+    for (let i = unit * 2; i <= boardSize; i += unit * 2) {
+      context.moveTo(i, 0);
+      context.lineTo(i, boardSize);
+
+      context.moveTo(0, i);
+      context.lineTo(boardSize, i);
+    }
+    
+    context.stroke();  
+    context.closePath();
+  }, []);
 
 
-
-export default function Board(){
   return (
-    <canvas width="750" height="750" />  
+    <CanvasBoard ref={canvasRef} width={boardSize} height={boardSize} />  
   )
 }
+
+export default Board;
