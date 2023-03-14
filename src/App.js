2c86ca8913c049ff6d5333135f63780cf9ce6769
brunosz19 <hesz.bruno05@gmail.com>
2023-03-14 02:22:02 -0500

    ADD: use the modals depends on the status of the game and use functions to save or change that status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useReducer } from "react";
 import Board from "./components/board";
-import { BOARD_SIZE, PLAYER_ONE, PLAYER_TWO, UNIT } from "./config/const";
+import GameResult from "./components/result";
+import Start from "./components/start";
+import {
+  BOARD_SIZE,
+  PLAYER_ONE,
+  PLAYER_TWO,
+  UNIT,
+  GAME_STATUS,
+} from "./config/const";
 import useInterval from "./hooks/useInterval";
 import getCellKey from "./utils/getCellKey";
 import getPlayableCells from "./utils/playableCells";
@@ -14,9 +22,18 @@ const initialState = {
     UNIT,
     players.map((player) => getCellKey(player.position.x, player.position.y))
   ),
+  gameStatus: GAME_STATUS.READY,
 };
 
 function updateGame(game, action) {
+  if (action.type === "start") {
+    return { ...initialState, gameStatus: GAME_STATUS.PLAYING };
+  }
+
+  if (action.type === "restart") {
+    return { ...initialState, gameStatus: GAME_STATUS.READY };
+  }
+
   if (action.type === "move") {
     const newPlayers = game.players.map((player) => ({
       ...player,
@@ -38,19 +55,23 @@ function updateGame(game, action) {
             .includes(myCellKey),
       };
     });
-    
-    const newOcupiedCells = game.players.map(player => getCellKey(player.position.x, player.position.y))
 
-    const playableCells = game.playableCells.filter(playableCell => {
+    const newOcupiedCells = game.players.map((player) =>
+      getCellKey(player.position.x, player.position.y)
+    );
+
+    const playableCells = game.playableCells.filter((playableCell) => {
       return !newOcupiedCells.includes(playableCell);
-    })
+    });
 
     return {
       players: newPlayersWithCollision,
       playableCells: playableCells,
+      gameStatus: newPlayersWithCollision.filter((player) => player.hasDied).length === 0
+      ? GAME_STATUS.PLAYING
+      : GAME_STATUS.ENDED
     };
   }
-
 
   if (action.type === "changeDirection") {
     const newPlayers = game.players.map((player) => ({
@@ -65,26 +86,34 @@ function updateGame(game, action) {
     return {
       players: newPlayers,
       playableCells: game.playableCells,
+      gameStatus: game.gameStatus,
     };
   }
 }
 
 function App() {
   const [game, gameDispatch] = useReducer(updateGame, initialState);
+  let result = null;
+  console.log(game.gameStatus)
 
-  const players = game.players;
-  const diedPlayers = players.filter(player => player.hasDied);
-  if (diedPlayers.length > 0) {
-    console.log(diedPlayers);
-  }
-
-  useInterval(() => {
-    gameDispatch({ type: "move" });
-  }, diedPlayers.length > 0 ? null : 100);
+  useInterval(
+    () => {
+      gameDispatch({ type: "move" });
+    },
+    game.gameStatus !== GAME_STATUS.PLAYING ? null : 100
+  );
 
   useEffect(() => {
     function handleKeyPress(event) {
       const key = `${event.keyCode}`;
+      if (key === "13") {
+        if (game.gameStatus === GAME_STATUS.READY) {
+          handleStart();
+        }
+        if (game.gameStatus === GAME_STATUS.ENDED) {
+          handleRestart();
+        }
+      }
       gameDispatch({ type: "changeDirection", key });
     }
 
@@ -93,11 +122,35 @@ function App() {
     return function cleanUp() {
       document.addEventListener("keydown", handleKeyPress);
     };
-  }, []);
+  }, [game.gameStatus]);
+
+  function handleStart() {
+    gameDispatch({ type: "start" });
+  }
+
+  function handleRestart() {
+    gameDispatch({ type: "restart" });
+  }
+
+  if (game.gameStatus === GAME_STATUS.ENDED) {
+    const winningPlayers = game.players.filter((player) => !player.hasDied);
+    if (winningPlayers.length === 0) {
+      result = "Empate";
+    } else {
+      result = `Ganador: ${winningPlayers.map(
+        (player) => `Jugador ${player.id}`
+      )}`;
+    }
+  }
 
   return (
     <div>
-      <Board players={game.players} />
+      <h1>CULEBRITA</h1>
+      <Board players={game.players} gameStatus={game.gameStatus} />
+      {game.gameStatus === GAME_STATUS.ENDED && (
+        <GameResult result={result} onClick={handleRestart} />
+      )}
+      {game.gameStatus === GAME_STATUS.READY && <Start onClick={handleStart} />}
     </div>
   );
 }
