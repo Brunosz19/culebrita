e1cc0fe01ba2d7733df3792d78e1fd4681b99048
brunosz19 <hesz.bruno05@gmail.com>
2023-03-14 02:21:08 -0500

    ADD: a styled component Instruction to show in the board the instructions and the method clearRect to clean the canvas path

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import React, { useEffect, useRef } from "react";
-import { BOARD_SIZE, UNIT } from "../config/const";
+import { BOARD_SIZE, GAME_STATUS, UNIT } from "../config/const";
 
 const CanvasBoard = styled.canvas`
   border: 1px solid #777;
@@ -9,26 +9,38 @@ const CanvasBoard = styled.canvas`
   margin-top: 25px;
 `;
 
-function Board({ players }) {
+const Instruction = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+function Board({ players, gameStatus }) {
   const canvasRef = useRef();
 
-  useEffect(function () {
-    const context = canvasRef.current.getContext("2d");
+  useEffect(
+    function () {
+      if (gameStatus === GAME_STATUS.READY) {
+        const context = canvasRef.current.getContext("2d");
+        context.clearRect(0, 0, BOARD_SIZE, BOARD_SIZE);
 
-    context.beginPath();
-    context.strokeStyle = "black";
+        context.beginPath();
+        context.strokeStyle = "black";
 
-    for (let i = UNIT * 2; i <= BOARD_SIZE; i += UNIT * 2) {
-      context.moveTo(i, 0);
-      context.lineTo(i, BOARD_SIZE);
+        for (let i = UNIT * 2; i <= BOARD_SIZE; i += UNIT * 2) {
+          context.moveTo(i, 0);
+          context.lineTo(i, BOARD_SIZE);
 
-      context.moveTo(0, i);
-      context.lineTo(BOARD_SIZE, i);
-    }
+          context.moveTo(0, i);
+          context.lineTo(BOARD_SIZE, i);
+        }
 
-    context.stroke();
-    context.closePath();
-  }, []);
+        context.stroke();
+        context.closePath();
+      }
+    },
+    [gameStatus]
+  );
 
   useEffect(
     function () {
@@ -41,7 +53,27 @@ function Board({ players }) {
     [players]
   );
 
-  return <CanvasBoard ref={canvasRef} width={BOARD_SIZE} height={BOARD_SIZE} />;
+  return (
+    <div>
+      <CanvasBoard ref={canvasRef} width={BOARD_SIZE} height={BOARD_SIZE} />
+      <Instruction>
+        {players.map((player) => {
+          return (
+            <div
+              style={{
+                color: player.color,
+                paddingLeft: "10px",
+                paddingRight: "10px",
+              }}
+              key={`player--${player.id}`}
+            >
+              {`${player.id}: ${player.instructions}`}
+            </div>
+          );
+        })}
+      </Instruction>
+    </div>
+  );
 }
 
 export default Board;
