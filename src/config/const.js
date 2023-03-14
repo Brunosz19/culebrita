737d67a1a3eb8494a10bae3c8f567026aa277b47
brunosz19 <hesz.bruno05@gmail.com>
2023-03-14 02:16:58 -0500

    ADD: prettier formatter, attribute instruction to give players the way to play and attribute GAME_STATUS to save the status of the game

--- src/config/const.js
+++ src/config/const.js
@@ -1,12 +1,17 @@
 const UNIT = 15;
 const BOARD_SIZE = 750;
+const GAME_STATUS = {
+  READY: 1,
+  PLAYING: 2,
+  ENDED: 3,
+};
 
 const DIRECTION = {
   LEFT: { x: -UNIT, y: 0 },
   RIGHT: { x: +UNIT, y: 0 },
   UP: { x: 0, y: -UNIT },
-  DOWN: { x: 0, y: UNIT }
-}
+  DOWN: { x: 0, y: UNIT },
+};
 
 const PLAYER_ONE = {
   color: "#CC0000",
@@ -15,11 +20,12 @@ const PLAYER_ONE = {
     38: DIRECTION.UP,
     39: DIRECTION.RIGHT,
     40: DIRECTION.DOWN,
-    37: DIRECTION.LEFT 
+    37: DIRECTION.LEFT,
   },
   direction: DIRECTION.DOWN,
-  position: { x: UNIT * 6, y: UNIT * 6 }
-}
+  position: { x: UNIT * 6, y: UNIT * 6 },
+  instructions: "Flechas de dirección",
+};
 
 const PLAYER_TWO = {
   color: "#0000CC",
@@ -28,10 +34,11 @@ const PLAYER_TWO = {
     87: DIRECTION.UP,
     68: DIRECTION.RIGHT,
     83: DIRECTION.DOWN,
-    65: DIRECTION.LEFT 
+    65: DIRECTION.LEFT,
   },
   direction: DIRECTION.UP,
-  position: { x: UNIT * 43, y: UNIT * 43 }
-}
+  position: { x: UNIT * 43, y: UNIT * 43 },
+  instructions: "Teclas AWSD",
+};
 
-export { UNIT, BOARD_SIZE, PLAYER_ONE, PLAYER_TWO }
+export { UNIT, BOARD_SIZE, PLAYER_ONE, PLAYER_TWO, GAME_STATUS };
