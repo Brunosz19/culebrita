a1e943cb875a93048c7ffe2c0995378057299f30
brunosz19 <hesz.bruno05@gmail.com>
2023-03-14 02:17:54 -0500

    ADD: modal Start that have and onClick to execute a function to start the game

---
+++ src/components/start.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Button from "./button";
+
+export const BeforeStart = styled.div(`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content:  center;
+  align-items: center;
+  color: #FFF;
+  background-color: #00000088;
+`);
+
+export default function Start({ onClick }) {
+  return (
+    <BeforeStart>
+      <Button onClick={onClick}>Comenzar</Button>
+    </BeforeStart>
+  );
+}
